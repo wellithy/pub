a0923bda72dd07de7a9f7ddcf4631bd7389009ee
Wael Ellithy <wellithy@yahoo.com>
2021-11-20 22:18:01 -0800

    Add README and more comments

--- src/main/kotlin/com/arxict/common/Cartesian.kt
+++ src/main/kotlin/com/arxict/common/Cartesian.kt
@@ -1,8 +1,19 @@
+/**
+ * Cartesian Product related functions.  Using Sequence instead of Set to allow duplicate.
+ * Some type aliases closely following the math naming:
+ *  * [Family][https://en.wikipedia.org/wiki/Indexed_family]
+ *  * [Tuple][https://en.wikipedia.org/wiki/Tuple]
+ *  * [CartesianProduct][https://en.wikipedia.org/wiki/Cartesian_product]
+ */
+
 package com.arxict.common
 
-typealias Family<T> = Sequence<T> // https://en.wikipedia.org/wiki/Indexed_family
-typealias Tuple = Sequence<Any?> // https://en.wikipedia.org/wiki/Tuple
-typealias CartesianProduct = Sequence<Tuple> // https://en.wikipedia.org/wiki/Cartesian_product
+typealias Family<T> = Sequence<T>
+typealias Tuple = Sequence<Any?>
+typealias CartesianProduct = Sequence<Tuple>
+
+private val emptyTuple: Tuple = emptySequence()
+private val zeroDCartesianProduct: CartesianProduct = sequenceOf(emptyTuple)
 
 fun <T> Family<T>.toCartesianProduct(tuple: Tuple): CartesianProduct =
     map(tuple::plus)
@@ -15,6 +26,3 @@ fun Sequence<Family<*>>.toCartesianProduct(): CartesianProduct =
 
 fun <T, U> Family<T>.cartesianProduct(other: Family<U>): Sequence<Pair<T, U>> =
     flatMap { other.map(it::to) }
-
-private val emptyTuple: Tuple = emptySequence()
-private val zeroDCartesianProduct: CartesianProduct = sequenceOf(emptyTuple)
